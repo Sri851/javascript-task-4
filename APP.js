e99fc2373e5d72452b04dbb4052a69544db4486f
Sreekanth Reddy <103722437+Sri851@users.noreply.github.com>
2022-11-13 09:35:42 +0530

    Add files via upload

---
+++ APP.js
@@ -0,0 +1,43 @@
+//Task4Q-1
+let m;
+const vacationSpots=['bancock','signapoor','japan'];
+console.log('bancock');
+console.log('sigapoor');
+console.log('japan');
+for(m=0;m<=10;m++){
+    console.log(vacationSpots);
+}
+
+//4-2task
+let num;
+for(num=5;num<=10;num++){
+    console.log(num);
+}
+//4-3task
+//here we use 'let' in side for loop also..and both normal order and reverse order here..
+for(let counter=0;counter<4;counter++){
+    console.log(counter);
+}
+for(let counter1=3;counter1>=0;counter1--){
+    console.log(counter1);
+}
+//4-4task
+let i;
+const vacationSpots1=['Bali','Paris','Tulum'];
+for(i=0;i<vacationSpots1.length;i++){
+    console.log("I would love to visit"+" " +vacationSpots1[i]) ;
+}
+//4-5task While Loop 
+let n=0;
+while(n<=5){
+    console.log(n++);
+}
+//4-6task DO While Loop Reverse
+let nume=5;
+do{
+    console.log(nume);
+    nume--;
+}while(nume>=0);
+
+
+
